d0c7592bd75ed6fe3d04d275db8434810ae892db
krawler <august.rafael@gmail.com>
2022-10-20 03:31:17 -0300

    metodo para buscar uma pessoa

--- controllers/PessoaController.js
+++ controllers/PessoaController.js
@@ -10,6 +10,18 @@ class PessoaController{
             return res.status(500).json(error.message)
         }    
     }
+
+    static async getOne(req, res){
+        const { id } = req.params;
+        try{
+            const people = await this.database.Pessoas.findOne({
+                 where: { id: Number(id)}
+            });
+            return res.status(200).json(people);
+        }catch(error){
+            return res.status(200).json(error);
+        }
+    }
 }
 
 module.exports = PessoaController
